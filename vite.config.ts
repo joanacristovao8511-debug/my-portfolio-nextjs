4a2f6b93b639936cf09e00f9d66dd17808b2c5c7
unknown <joanacristovao8511@gmail.com>
2026-09-20 20:53:35 +0800

    modify configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,19 @@
 import { defineConfig } from "vite";
 import vinext from "vinext";
 import { cloudflare } from "@cloudflare/vite-plugin";
-import { responseStoreAdapter } from "@vinext/cloudflare/cache/response-store-adapter";
 import { imagesOptimizer } from "@vinext/cloudflare/images/images-optimizer";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
+
     vinext({
-      cache: responseStoreAdapter(),
-      images: { optimizer: imagesOptimizer() },
+      images: {
+        optimizer: imagesOptimizer(),
+      },
     }),
+
     cloudflare({
       viteEnvironment: {
         name: "rsc",
